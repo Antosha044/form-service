c03d1b0358d450d9be5c3f5548f3fd2b801ddb40
Anton Kostil <v999253@gmail.com>
2025-05-29 19:37:14 +0300

    add question to models.py

--- src/models/models.py
+++ src/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, text, Enum as SQLEnum
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, text, Enum as SQLEnum, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import UUID
 from src.core.database import Base
@@ -34,7 +34,19 @@ class Form(Base):
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
     is_anonymous: Mapped[bool] = mapped_column(default=False, nullable=False)
-    status: Mapped[Status] = mapped_column(SQLEnum(Status, name="status_enum"),nullable=False)
+    status: Mapped[Status] = mapped_column(SQLEnum(Status, name="status_enum"), nullable=False)
 
     owner: Mapped["User"] = relationship(back_populates = "forms")
-    
+    questions: Mapped[list["Question"]] = relationship(back_populates = "form")
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    id: Mapped[uuidpk]
+    form_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("forms.id", ondelete="CASCADE"), nullable=False)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+    question_type: Mapped[Question_type] = mapped_column(SQLEnum(Question_type, name="question_type_enum"), nullable=False)
+    order: Mapped[int] = mapped_column(nullable=False, default=1)
+
+    form: Mapped["Form"] = relationship(back_populates="questions")
+
