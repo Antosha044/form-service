bcaa9cc6e82e3cf80895ed38f5260d0c00beb00a
Anton Kostil <v999253@gmail.com>
2025-06-04 21:07:05 +0300

    add jwt_utils.py

---
+++ src/core/jwt_utils.py
@@ -0,0 +1,27 @@
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from typing import
+from src.settings import settings
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise Exception("Token expired")
+    except jwt.JWTError:
+        raise Exception("Invalid token")
