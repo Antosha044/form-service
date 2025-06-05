38857f2517b9e444efba1c9849d19f046eca4063
Anton Kostil <v999253@gmail.com>
2025-06-05 21:49:30 +0300

    add get_me and get_all to user_router

--- src/routers/user_router.py
+++ src/routers/user_router.py
@@ -6,15 +6,23 @@ from src.core.jwt_utils import create_access_token
 from src.schemas.user import UserOut, UserUpdate
 from src.crud import user as user_crud
 from src.core.database import get_db
+from src.core.dependencies import get_current_user
 from src.models.models import User
 
 router = APIRouter(prefix="/users", tags=["users"])
 
+@router.get("/me", response_model=UserOut)
+async def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
 @router.get("/{user_id}", response_model=UserOut)
-async def get_user_by_id(user_id: UUID, session: AsyncSession = Depends(get_db)):
+async def get_user_by_id(user_id: UUID, session: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
     user = await user_crud.get_user_by_id(user_id, session)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
 
+@router.get("/", response_model=list[UserOut])
+async def get_all_users(session: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await user_crud.get_all_users(session)
 
